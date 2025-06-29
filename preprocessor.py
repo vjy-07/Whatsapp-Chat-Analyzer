8200a9cf7a0082056da188979ad623f5c551014a
vjy-07 <gvijaycrl@gmail.com>
2025-06-29 19:48:45 +0530

    date format updated

--- preprocessor.py
+++ preprocessor.py
@@ -1,15 +1,26 @@
 import re
 import pandas as pd
+from datetime import datetime
+
+def parse_datetime(date_str):
+    formats = ['%d/%m/%Y, %H:%M - ', '%m/%d/%y, %H:%M - ', '%d/%m/%y, %H:%M - ']
+    for fmt in formats:
+        try:
+            return datetime.strptime(date_str, fmt)
+        except ValueError:
+            continue
+    return pd.NaT
 
 def preprocess(data):
-    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
 
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
 
     df = pd.DataFrame({'user_message': messages, 'message_date': dates})
-    # convert message_date type
-    df['message_date'] = pd.to_datetime(df['message_date'], format='%m/%d/%y, %H:%M - ')
+
+    # Apply custom parser to handle multiple datetime formats
+    df['message_date'] = df['message_date'].apply(parse_datetime)
     df.rename(columns={'message_date': 'date'}, inplace=True)
 
     users = []
@@ -27,7 +38,6 @@ def preprocess(data):
     df['user'] = users
     df['message'] = messages
     df.drop(columns=['user_message'], inplace=True)
-    df.head()
 
     df['only_date'] = df['date'].dt.date
     df['year'] = df['date'].dt.year
@@ -37,15 +47,15 @@ def preprocess(data):
     df['day_name'] = df['date'].dt.day_name()
     df['hour'] = df['date'].dt.hour
     df['minute'] = df['date'].dt.minute
-    
+
     period = []
-    for hour in df[['day_name', 'hour']]['hour']:
+    for hour in df['hour']:
         if hour == 23:
-            period.append(str(hour) + "-" + str('00'))
+            period.append(f"{hour}-00")
         elif hour == 0:
-            period.append(str('00') + "-" + str(hour + 1))
+            period.append("00-1")
         else:
-            period.append(str(hour) + "-" + str(hour + 1))
+            period.append(f"{hour}-{hour+1}")
 
     df['period'] = period
 
